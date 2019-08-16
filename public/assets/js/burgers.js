43b9dd1c789ae946bb4b550c4f31de9de44a6172
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-08-16 16:11:00 -0400

    updated files with few changes with views and controllers

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -27,7 +27,7 @@ $(function() {
   
       var newBurger = {
         burger_name: $("#bg").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim()
+        devoured: 0
       };
   
       // Send the POST request.
